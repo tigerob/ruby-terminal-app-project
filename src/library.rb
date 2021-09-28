ecb592fa996cca1e409611b8f3e580af51c9ace8
tigerob <obrienxyz@gmail.com>
2021-09-28 21:04:43 +1000

    Add skeleton of methods for menu option 2.

--- src/library.rb
+++ src/library.rb
@@ -3,3 +3,5 @@ require_relative "./Book.rb"
 @tolstoy = Book.new("Leo Tolstoy - War and Peace (1867)", {:recency => "old", :geography => "world"}, ["historical", "philosophical"])
 
 @morrison = Book.new("Toni Morrison - Song of Solomon (1977)", {:recency => "new", :geography => "american"}, ["African-American literature", "magical realism"])
+
+@joyce = Book.new("James Joyce - Ulysses (1920)", {:recency => "old", :geography => "world"}, ["modernist literature"])
--- src/main.rb
+++ src/main.rb
@@ -1,12 +1,13 @@
 require_relative "./library.rb"
 
-all_books = [@tolstoy, @morrison]
+all_books = [@tolstoy, @morrison, @joyce]
 
 # users_chosen_books = []
 
-# Method for menu screen
+# Method for menu
 def display_menu_and_take_users_choice(all_books)
-    puts "\nWelcome to 21 Books You’ve Been Meaning To Read. Etc. Etc. \n\nHow would you like to choose a book? Select from the following options and enter a number to continue:\n\n1.   Choose from the full list of books\n2.   Choose by genre\n3.   Take the 'what do I feel like?' quiz\n4.   Spin the wheel for a random book\n5.   I'm done for now - exit\n\n"
+    system("clear")
+    puts "Welcome to 21 Books You’ve Been Meaning To Read. Etc. Etc. \n\nHow would you like to choose a book? Select from the following options and enter a number to continue:\n\n1.   Choose from the full list of books\n2.   Choose by genre\n3.   Take the 'what do I feel like?' quiz\n4.   Spin the wheel for a random book\n5.   I'm done for now - exit\n\n"
     users_choice = gets.strip.to_i # raise exception if not an integer value 1-4?
     case users_choice
         when 1
@@ -15,7 +16,7 @@ def display_menu_and_take_users_choice(all_books)
             #insert method to choose a book from the array and add it to users_chosen_books array
         when 2
             system("clear")
-            display_all_genres_and_take_users_choice
+            display_all_genres_and_take_users_choice(all_books)
 
             #insert method to choose a book from the array and add it to users_chosen_books array
         when 3
@@ -38,19 +39,18 @@ def display_all_books(all_books)
 end
 
 # Methods for menu option 2
-def display_all_genres_and_take_users_choice
+def display_all_genres_and_take_users_choice(all_books)
     puts "Select from the following genres and enter a number to continue:\n\n1.   Adventure\n2.   African-American literature\n3.   Coming of age\n4.   Drama\n5.   Dystopian\n6.   Fantasy\n7.   Historical\n8.   Magical realism\n9.   Memoir\n10.  Modernist literature\n11.  Philosophical\n12.  Satire\n13.  Science fiction\n\n"
     users_choice = gets.strip.to_i # raise exception if not an integer value 1-13?
     case users_choice
         when 1
-            all_books.each do |book|
-                if genres.include?("historical")
-                    puts book.book_details
-                end
-            end 
 
         when 2
-
+            all_books.each do |book|
+                if @genres.include?("African-American literature")
+                    puts book.book_details
+                end
+            end
         when 3
 
         when 4
@@ -76,10 +76,6 @@ def display_all_genres_and_take_users_choice
     end
 end
 
-# def 
-#     
-
-# end
 
 
 # Methods that run app
