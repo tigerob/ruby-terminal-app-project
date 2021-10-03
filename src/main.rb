3bb55730f978c1a9e93bb82c6c7c0913c57a0d25
tigerob <obrienxyz@gmail.com>
2021-10-04 00:13:48 +1100

    Add Colorize and Artii gems and incorporate into code.

--- src/main.rb
+++ src/main.rb
@@ -1,10 +1,15 @@
 require_relative "./library.rb"
+require "colorize"
+require "artii"
+
+title = Artii::Base.new
 
 all_books = [@tolstoy, @morrison, @joyce, @zafon, @tolkien, @rushdie, @cervantes, @pullman, @heller, @orwell, @hosseini, @alcott, @mitchell, @rand, @wilde, @nabokov, @stockett, @karr, @melville, @pynchon, @atwood]
 
+
 # -------------------- Method for menu --------------------
-def display_menu_and_take_users_choice(all_books)
-    puts "Welcome to 21 Books You’ve Been Meaning To Read - the terminal app that solves your dilemma of choosing which book to read.\n\nIn today's world, some people feel they are spoilt for choice, but are paralysed when it comes to making a decision. If that's you, look no further. This app knows Penguin Random House’s 'The 21 Books You've Been Meaning To Read' and will help you find your next nourshing page-turner.\n\nHow would you like to choose a book? Select from the following options and enter a number to continue:\n\n1.   Choose from the full list of books\n2.   Choose by genre\n3.   Take the 'what do I feel like?' quiz\n4.   Spin the wheel for a random book\n5.   I'm done for now - exit\n\n"
+def run_app(all_books)
+    puts "Welcome to 21 Books You’ve Been Meaning To Read (21 BYBMTR) - the terminal app that solves your dilemma of choosing which book to read.\n\nIn today's world, some people feel they are spoilt for choice, but are paralysed when it comes to making a decision. If that's you, look no further. This app knows Penguin Random House’s 'The 21 Books You've Been Meaning To Read' and will help you find your next nourshing page-turner.\n\nHow would you like to choose a book? Select from the following options and enter a number to continue:\n\n1.   Choose from the full list of books\n2.   Choose by genre\n3.   Take the 'what do I feel like?' quiz\n4.   Spin the wheel for a random book\n5.   I'm done for now - exit\n\n"
     users_choice = gets.strip.to_i
     case users_choice
         when 1
@@ -23,11 +28,10 @@ def display_menu_and_take_users_choice(all_books)
             system("clear")
             exit_app("Thank you for visiting. Happy reading!")
     end
-    display_book_art
     raise if ![1, 2, 3, 4, 5].include?(users_choice)
     rescue
         system("clear")
-        puts "-------------------------------------------------------------------\n\nPlease type in a number from 1 to 4 to proceed! Here we go again :)\n\n-------------------------------------------------------------------\n\n"
+        puts "-------------------------------------------------------------------\n\nPlease type in a number from 1 to 4 to proceed!\n\n-------------------------------------------------------------------\n\n".colorize(:red)
         retry
 end
 
@@ -35,7 +39,7 @@ end
 def display_all_books(all_books)
     puts "Take your pick! Here's the full list:\n\n"
     all_books.each do |book|
-        puts book.book_details
+        puts book.book_details.colorize(:light_blue).bold
     end
 end
 
@@ -49,86 +53,86 @@ def display_all_genres_and_take_users_choice(all_books)
         when 1
             all_books.each do |book|
                 if book.genres.include?("adventure")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 2
             all_books.each do |book|
                 if book.genres.include?("african-american literature")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 3
             all_books.each do |book|
                 if book.genres.include?("coming of age")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 4
             all_books.each do |book|
                 if book.genres.include?("drama")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 5
             all_books.each do |book|
                 if book.genres.include?("dystopian")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 6
             all_books.each do |book|
                 if book.genres.include?("fantasy")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 7
             all_books.each do |book|
                 if book.genres.include?("historical")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 8
             all_books.each do |book|
                 if book.genres.include?("magical realism")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 9
             all_books.each do |book|
                 if book.genres.include?("memoir")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 10
             all_books.each do |book|
                 if book.genres.include?("modernist literature")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 11
             all_books.each do |book|
                 if book.genres.include?("philosophical")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 12
             all_books.each do |book|
                 if book.genres.include?("satire")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
         when 13
             all_books.each do |book|
                 if book.genres.include?("science fiction")
-                    puts book.book_details
+                    puts book.book_details.colorize(:light_blue).bold
                 end
             end
     end
     raise if ![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].include?(users_choice)
     rescue
         system("clear")
-        puts "--------------------------------------------------------------------\n\nPlease type in a number from 1 to 13 to proceed! Here we go again :)\n\n--------------------------------------------------------------------\n\n"
+        puts "--------------------------------------------------------------------\n\nPlease type in a number from 1 to 13 to proceed!\n\n--------------------------------------------------------------------\n\n".colorize(:red)
         retry
 end
 
@@ -153,7 +157,7 @@ def quiz_q_1(quiz_answers)
     rescue
         quiz_answers.pop()
         system("clear")
-        puts "---------------------------------------------------------------------------\n\nPlease type in either 'new' or 'old' for this question! Here it is again :)\n\n---------------------------------------------------------------------------\n\n"
+        puts "---------------------------------------------------------------------------\n\nPlease type in either 'new' or 'old' for this question!\n\n---------------------------------------------------------------------------\n\n".colorize(:red)
         retry
 end
 
@@ -165,7 +169,7 @@ def quiz_q_2(quiz_answers)
     rescue
         quiz_answers.pop()
         system("clear")
-        puts "----------------------------------------------------------------------------------\n\nPlease type in either 'American' or 'world' for this question! Here it is again :)\n\n----------------------------------------------------------------------------------\n\n"
+        puts "----------------------------------------------------------------------------------\n\nPlease type in either 'American' or 'world' for this question!\n\n----------------------------------------------------------------------------------\n\n".colorize(:red)
         retry
 end
 
@@ -189,7 +193,7 @@ def quiz_q_3(quiz_answers, all_books, quiz_q_3_genres)
     puts "Here's some piping-hot literature that is right up your alley:\n\n"
     all_books.each do |book|
         if book.recency_and_geography[:recency] == quiz_answers[0] && book.recency_and_geography[:geography] == quiz_answers[1] && book.genres.include?(quiz_answers[2])
-            puts book.book_details
+            puts book.book_details.colorize(:light_blue).bold
         end
     end
 
@@ -197,7 +201,7 @@ def quiz_q_3(quiz_answers, all_books, quiz_q_3_genres)
     rescue
         quiz_answers.pop()
         system("clear")
-        puts "------------------------------------------------------------------------------\n\nPlease type in one of the genres listed for this question! Here it is again :)\n\n------------------------------------------------------------------------------\n\n"
+        puts "------------------------------------------------------------------------------\n\nPlease type in one of the genres listed for this question!\n\n------------------------------------------------------------------------------\n\n".colorize(:red)
         retry
 end
 
@@ -206,7 +210,8 @@ end
 
 def display_random_book(all_books)
     random_book = all_books.sample
-    puts "The wheel has spoken:\n\n#{random_book.book_details}"
+    puts "The wheel has spoken:\n\n"
+    puts "#{random_book.book_details}".colorize(:light_blue).bold
 end
 
 
@@ -220,7 +225,7 @@ def display_book_art
     "  //__...--~~~~~~-._  |  _.-~~~~~~--...__\\\\\n"\
     " //__.....----~~~~._\\ | /_.~~~~----.....__\\\\\n"\
     "====================\\\\|//====================\n"\
-    "                    `---`\n\n"
+    "                    `---`\n".colorize(:light_white)
     puts "Press enter to return to the start menu.\n\n\n"
     users_choice = gets
     raise if !["\n"].include?(users_choice)
@@ -242,5 +247,7 @@ end
 
 while true
     system("clear")
-    display_menu_and_take_users_choice(all_books)
+    puts title.asciify("21      BYBMTR!")
+    run_app(all_books)
+    display_book_art
 end
